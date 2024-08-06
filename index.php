dcffb541780163025737ad5f9d19d190a45e1b8d
Jhon Sanchez <henryjhon10@gmail.com>
2024-08-06 10:39:29 -0500

    expicacion en el index

---
+++ index.php
@@ -0,0 +1,8 @@
+<h1> imagenes en la carpeta img</h1>
+
+
+<p> Calificador Jhon Hnery Sanchez</p>
+
+<h5> La expocicion del grupo de Newwins estuvo buena la exposicion pero se puede mejorara
+    mi claificacion 8/10
+</h5>
